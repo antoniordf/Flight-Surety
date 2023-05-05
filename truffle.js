959816586aed40b6bfbb856360eff502d4b74569
Antonio Furtado <antoniordf@hotmail.com>
2023-05-05 16:22:56 +0100

    Further updates to truffle config file. Installed dotenv and created environment variables

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const { MNEMONIC, PROJECT_ID } = process.env;
+
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const mnemonic =
   "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
@@ -5,12 +8,26 @@ const mnemonic =
 module.exports = {
   networks: {
     development: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: "*",
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 9545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
+
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: MNEMONIC,
+          },
+          providerOrUrl: `https://sepolia.infura.io/v3/${PROJECT_ID}`,
+        }),
+      network_id: 11155111,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
+
   compilers: {
     solc: {
       version: "^0.8.19",
